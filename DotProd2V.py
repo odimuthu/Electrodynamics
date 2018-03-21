38d2cedee192bee40c87d56aa5918fc331f4113e
odimuthu <37123131+odimuthu@users.noreply.github.com>
2018-03-20 21:14:26 -0400

    new

--- DotProd2V.py
+++ DotProd2V.py
@@ -8,13 +8,13 @@ def dotprod2v(a, b, theta):
     :param b: Magnitude of second vector in Newtons
     :param theta: Angle between degrees
     """
-    a * b * math.cos(math.radians(theta))  # Dot product function
+    return a * b * math.cos(math.radians(theta))  # Dot product function
 
 
 A = int(input("Enter the magnitude of first vector in Newtons: "))
 B = int(input("Enter the magnitude of second vector in Newtons: "))
 Theta = int(input("Enter the angle between vectors in degrees: "))
 
-d2p = A * B * math.cos(math.radians(Theta))  # Calculate dot product
+d2p = dotprod2v(A, B, Theta)
 
 print("The dot-product is", d2p)  # Print dot product
